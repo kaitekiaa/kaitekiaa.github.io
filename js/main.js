177954bcc8430babaa6558585bf53fb7667914ad
makomsh <matsui.mako@g.wakayama-u.jp>
2016-12-06 17:51:18 +0900

    test

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(function(){
 	var img = document.getElementById('sampling');
 
 	RGBaster.colors(img, {
-	 					//範囲
+	  paletteSize: 600,					//範囲
 	  exclude: [ 'rgb(255,255,255)' ],  // 除外した色
 	  success: function(payload){
 	    console.log(payload.dominant);
