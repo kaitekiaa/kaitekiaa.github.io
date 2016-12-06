b8312e5585eccfa0ab7172baf80ca28c1ae4faf6
makomsh <matsui.mako@g.wakayama-u.jp>
2016-12-06 17:39:14 +0900

    test

--- js/main.js
+++ js/main.js
@@ -3,10 +3,12 @@ $(function(){
 	var img = document.getElementById('sampling');
 
 	RGBaster.colors(img, {
-	  paletteSize: 30,
-	  exclude: [ 'rgb(255,255,255)' ],  // don't count white
+	  paletteSize: 30,					//範囲
+	  exclude: [ 'rgb(255,255,255)' ],  // 除外した色
 	  success: function(payload){
-	    // do something with payload
+	    console.log(payload.dominant);
+        console.log(payload.secondary);
+        console.log(payload.palette);
 	  }
 	})
 
